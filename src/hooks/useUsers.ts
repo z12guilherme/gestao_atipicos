476d9cdb66621859dcfa2b8ea9f687578dfe1779
unknown <mguimarcos39@gmail.com>
2025-12-16 16:21:58 -0300

    Ajustando configurações de senha de Usuários

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -115,7 +115,7 @@ export function useUsers() {
   const sendPasswordReset = useMutation({
     mutationFn: async (email: string) => {
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${window.location.origin}/update-password`,
+        redirectTo: 'https://gestao-atipicos.vercel.app/update-password',
       });
 
       if (error) {
