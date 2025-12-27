20fad7f0cc0ca78859e5196ea70f258845338bf1
Marcos Guilherme <mguimarcos39@gmail.com>
2025-12-27 12:40:01 -0300

    Ajustes VERCEL

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => ({
     mode === "development" && componentTagger(),
     VitePWA({
       registerType: "autoUpdate",
+      manifestFilename: "manifest.json",
       devOptions: {
         enabled: true,
       },
@@ -41,12 +42,12 @@ export default defineConfig(({ mode }) => ({
         orientation: "portrait",
         icons: [
           {
-            src: "icons/icon-192.png",
+            src: "/icons/icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "icons/icon-512.png",
+            src: "/icons/icon-512.png",
             sizes: "512x512",
             type: "image/png"
           }
