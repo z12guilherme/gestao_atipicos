926925461505e3192de3781d9efd18555d5d3c20
Marcos Guilherme <mguimarcos39@gmail.com>
2025-12-27 12:18:24 -0300

    Ajustes VERCEL

--- vite.config.ts
+++ vite.config.ts
@@ -43,14 +43,12 @@ export default defineConfig(({ mode }) => ({
           {
             src: "icons/icon-192.png",
             sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable"
+            type: "image/png"
           },
           {
             src: "icons/icon-512.png",
             sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
+            type: "image/png"
           }
         ]
       }
